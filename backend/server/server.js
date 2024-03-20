ce4be7c7f10fb070c731a854297412d327e193d7
Ranu20001 <it22168436@my.sliit.lk>
2024-03-20 21:59:19 +0530

    update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,5 +1,12 @@
+const express = require('express')
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://bhawan:200132400588@atlascluster.fl5bp73.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
-.then(() => console.log('Connected'))
+const app = express()
+
+const mongoDbURL = "mongodb+srv://bhawan:200132400588@atlascluster.fl5bp73.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster"
+
+mongoose.connect(mongoDbURL)
+.then(() => console.log('Database Connected'))
 .catch((err) => console.log(err))
+
+app.listen(3000, () => console.log('Sever Connected'))
