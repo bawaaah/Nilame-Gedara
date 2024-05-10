a582a87f0ebf8f55d263ef4661cc74ebfdbb57e9
VKaushalya03 <165917431+VKaushalya03@users.noreply.github.com>
2024-05-10 20:53:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+/*import all components*/
+import Login from './components/Login' ;
+import Register from './components/Register';
+import Reset from './components/Reset';
+import Profile from './components/Profile';
+import PageNotFound from './components/PageNotFound';
+import Home from './components/Home';
+import Users from './components/Users';
+import Email from './components/Email';
+
+/**auth middleware */
+import { AuthorizeUser } from './middleware/auth'
+
+
+/*root routes */
+const router = createBrowserRouter([
+  {
+    path : '/login',
+    element : <Login></Login>
+  },
+  {
+    path : '/register',
+    element : <Register></Register>
+  },
+  {
+    path : '/profile',
+    element : <AuthorizeUser> <Profile></Profile></AuthorizeUser>
+  },
+  {
+    path : '/reset/:token',
+    element : <Reset></Reset>
+  },
+  {
+    path : '*',
+    element : <PageNotFound></PageNotFound>
+  },
+  {
+    path : '/',
+    element : <Home></Home>
+  },
+  {
+    path : '/users',
+    element : <Users></Users>
+  },
+  {
+    path : '/email',
+    element : <Email></Email>
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 
