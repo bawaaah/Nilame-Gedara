b96bcc48d679c55d37b5c02322b5e687f780d06d
nethmigune2 <nethmi20020622@gmail.com>
2024-05-10 17:30:16 +0530

    names

--- backend/routes/incident.route.js
+++ backend/routes/incident.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Incident = require('../model/incident.model');
+const Incident = require('../models/incident.model');
 
 // Create a new incident
 router.post('/', async (req, res) => {
